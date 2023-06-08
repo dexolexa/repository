5f4a1cda5ffa5022a6bc3b58bf89863a2d70dc9e
dexolexa <oleksiy.platov@yandex.ru>
2023-06-08 12:04:18 +0300

    Update some commits.py

--- some commits.py
+++ some commits.py
@@ -1,1 +1,38 @@
-print('yello')
+"""
+This is a echo bot.
+It echoes any incoming text messages.
+"""
+
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '6256551450:AAFuY19eE2Eb5y1-I8KyXfD3yYBQqCF-C70'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    # old style:
+    # await bot.send_message(message.chat.id, message.text)
+
+    await message.answer(message.text+ f"halooo {message.from_user['username']}")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
