726e869130c093c9ecb8607df0f528739825d941
dexolexa <oleksiy.platov@yandex.ru>
2023-06-12 10:01:46 +0300

    bit by bit

--- anonymous_bot.py
+++ anonymous_bot.py
@@ -1,31 +1,159 @@
 import logging
+from typing import Iterable, Optional
 
-from aiogram import Bot, Dispatcher, executor, types
-
-from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, \
-    InlineKeyboardButton, Message
-
-from aiogram.dispatcher import FSMContext
+from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.types import Message
+from aiogram.utils import executor
+from dataclasses import dataclass, field
 
-from env import API_TOKEN
+from env import TOKEN
 
-# Initialize bot and dispatcher
-bot = Bot(token=API_TOKEN)
+
+@dataclass
+class Dialog:
+    name: str
+    users: Optional[set[int]] = field(default_factory=lambda: set())
+
+    def not_me(self, me: int) -> set[int]:
+        return self.users - {me}
+
+    def add_user(self, user_id: int):
+        self.users.add(user_id)
+
+    def remove_user(self, user_id: int):
+        self.users.remove(user_id)
+
+    def __str__(self):
+        return f"{self.name} ({len(self.users)})"
+
+
+# Настройа логгирования
+logging.basicConfig(level=logging.INFO)
+
+# Инициализировать бота и задать диспетчера
+bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
-@dp.message_handler(commands='start', state='*')
-async def _(message: Message, state: FSMContext):
-    state.update_data({'user_id': message.from_user.id})
-    await message.answer('Привет, Напиши мне что нибудь!')
-    await state.set_state('wait')
+all_users = set()
 
-@dp.message_handler(state='wait')
-async def _(message: Message, state: FSMContext):
-    id2  = message.from_user.id
-    text = f"@{message.from_user.username} написал(а):\n message.text"
+dialogs: list[Dialog] = []
 
+
+@dp.message_handler(commands=['start'], state="*")
+async def _(message: Message, state: FSMContext):
+    user_id = message.from_id
+
+    # Запоминаем всех пользователей, что нажали /start
+    if user_id not in all_users:
+        all_users.add(user_id)
+        await send_to_users(f"Всего пользователей: {len(all_users)}")
+
+    await state.update_data({"user_id": user_id,
+                             "username": message.from_user.username})
+    await message.answer("Привет! Напиши мне что-нибудь!")
+    # то же самое что и
+    # await bot.send_message(message.from_id, "Привет! Напиши мне что-нибудь!")
+    await state.set_state("list_dialogs")
+
+
+@dp.message_handler(state="list_dialogs")
+async def _(message: Message, state: FSMContext):
+    await list_dialogs(message)
+    await state.set_state("setup_dialog")
+
+
+async def list_dialogs(message: Message):
+    text = "Текущие диалоги: "
+    text += ",".join(map(str, dialogs))
     await message.answer(text)
+    await message.answer("Выберите нужный диалог (напишите его имя, либо 'new' )")
+
+
+@dp.message_handler(state="setup_dialog")
+async def _(message: Message, state: FSMContext):
+    name_of_dialog = message.text.strip()
+
+    if name_of_dialog == 'new':
+        await message.answer("Введите название для диалога")
+        await state.set_state("create_dialog")
+        return
+    elif name_of_dialog == 'update':
+        await list_dialogs(message)
+
+    chosen_dialog = None
+
+    for dialog in dialogs:
+        if name_of_dialog == dialog.name:
+            chosen_dialog = dialog
+            break
+
+    if chosen_dialog:
+        await message.answer(f"Вы выбрали диалог: {chosen_dialog}")
+        await chose_dialog(message.from_id, chosen_dialog)
+        await state.update_data({"dialog": chosen_dialog})
+        await state.set_state("wait_for_message")
+    else:
+        await message.answer("Вы не ввели существующего диалога")
+        await list_dialogs(message)
+
+
+async def chose_dialog(user_id: int, chosen_dialog: Dialog):
+    text = f"Пользователь {user_id} вышел из этого диалога."
+    for dialog in dialogs:
+        if dialog is chosen_dialog:
+            continue
+        if user_id in dialog.users:
+            dialog.remove_user(user_id)
+            await send_to_users(text, dialog.users)
+
+    chosen_dialog.add_user(user_id)
+    text = f"Пользователь {user_id} добавился в этот диалог."
+    await send_to_users(text, chosen_dialog.not_me(user_id))
+
+
+@dp.message_handler(state="create_dialog")
+async def _(message: Message, state: FSMContext):
+    name_of_dialog = message.text
+    new_dialog = Dialog(name=name_of_dialog)
+    dialogs.append(new_dialog)
+    await chose_dialog(message.from_id, new_dialog)
+    await message.answer(f"Вы создали новый диалог {new_dialog}. Вы находитесь в нём")
+    await state.update_data({"dialog": new_dialog})
+    await state.set_state("wait_for_message")
+
+
+@dp.message_handler(state="wait_for_message")
+async def _(message: Message, state: FSMContext):
+    # await message.answer("Спасибо за сообщение!")
+    # id_ = message.from_id # постоянный для аккаунта
+    # message.from_user.username # пользователь может сменить его
+    # text = f"@{message.from_user.username} написал(а):\n{message.text}"
+
+    text = \
+        f"<a href='tg://user?id={message.from_id}'>" \
+        f"пользователь</a> написал(а):\n{message.text}"
+
+    my_data = await state.get_data()
+    dialog = my_data['dialog']
+    if not dialog:
+        await message.answer("Вы не находитесь в диалоге")
+    all_except_me = dialog.not_me(message.from_id)
+    await send_to_users(text, users=all_except_me, parse_mode="HTML")
+
+    # await message.answer(text, parse_mode="HTML")
+
+
+async def send_to_users(text, users: Iterable[int] = None, parse_mode=None):
+    if users is None:
+        users = tuple(all_users)
+    else:
+        users = tuple(users)
+
+    for user_id in users:
+        await bot.send_message(user_id, text, parse_mode=parse_mode)
+
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True, on_startup=())
+    executor.start_polling(dp, skip_updates=True)
--- some commits.py
+++ some commits.py
@@ -1,3 +1,4 @@
+# нужные библиотеки
 import logging
 
 from aiogram import Bot, Dispatcher, executor, types
@@ -10,34 +11,109 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from env import API_TOKEN
 
-# Configure logging
+# логи
 logging.basicConfig(level=logging.INFO)
 
-# Initialize bot and dispatcher
+# сам бот
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
-replies = ReplyKeyboardMarkup(resize_keyboard=True)
 
-kb = InlineKeyboardMarkup(row_width=2)
-inb1 = InlineKeyboardButton("(работа или/и игры)пк", callback_data='b1')
-inb2 = InlineKeyboardButton("консоль(игры)", callback_data='b2')
-inb3 = InlineKeyboardButton("создание музыки, программирование...", callback_data='b3')
-inb4 = InlineKeyboardButton("Vr шлем", callback_data='b4')
+#кнопки
+
+inb1 = InlineKeyboardButton("(работа или/и игры)пк", callback_data='pc')
+inb2 = InlineKeyboardButton("консоль(игры)", callback_data='cons')
+inb3 = InlineKeyboardButton("создание музыки, программирование...", callback_data='mac')
+inb4 = InlineKeyboardButton('ВР ШЛЕМЫ', callback_data='vrhelm')
+
 nb3 = KeyboardButton(text="геолокация и контакт")
 nb4 = KeyboardButton(text="помощь с выбором")
-kb.add(inb2).add(inb1)  # .add(inb3).add(inb4)
+
 geoloc = KeyboardButton(text="контакт", request_contact=True)
 contact = KeyboardButton(text="геолокация", request_location=True)
-contact2 = KeyboardButton(text="Вернуться?", callback_data='back')
-gckb= ReplyKeyboardMarkup(resize_keyboard=True).insert(geoloc).insert(contact)
+contact2 = KeyboardButton(text="Вернуться?")
 
-replies.add(nb4).add(nb3)
+# console1 = InlineKeyboardButton('Портативные',callback_data="port")
+# console2 = InlineKeyboardButton('Домашние',callback_data="home")
+#
+# portative1 = InlineKeyboardButton("Nintendo Switch", url="")
+# portative2 = InlineKeyboardButton("", url="")
+#
+# ps5 = InlineKeyboardButton("", url="")
+# xbox_x = InlineKeyboardButton("", url="")
+#
+# vr_s = InlineKeyboardButton("", url="")
+# pc_vr = InlineKeyboardButton("", url="")
+#
+# pico_vr = InlineKeyboardButton("", url="")
+# meta_q = InlineKeyboardButton("", url="")
+#
+# v_index = InlineKeyboardButton("", url="")
+# htc_v = InlineKeyboardButton("", url="")
+# rift = InlineKeyboardButton("", url="")
+#
+# macbook = InlineKeyboardButton('макбук', url="https://www.dns-shop.ru/catalog/recipe/8ddf1df79c19c23d/macbook/")
+#
+# gamingpc = InlineKeyboardButton("", url="")
+#
+# gamingandworkin = InlineKeyboardButton("", url="")
+#
+# workinpc = InlineKeyboardButton("", url="")
 
 
+# само нахождение кнопок
+
+kb = InlineKeyboardMarkup(row_width=2).add(inb2).add(inb1).add(inb3).add(inb4)
+
+# consolekb = InlineKeyboardMarkup(row_width=1).add(console1).add(console2)
+
+# pkb = InlineKeyboardMarkup(row_width=1).add().add()
+
+# hkb = InlineKeyboardMarkup(row_width=1).add().add()
+
+# vrkb = InlineKeyboardMarkup(row_width=1).add(vr_s).add(pc_vr)
+
+# svr=InlineKeyboardMarkup(row_width=1).add().add(pico_vr)
+
+# macbookkb = InlineKeyboardMarkup(row_width=1).add(macbook)
+
+# pckb = InlineKeyboardMarkup(row_width=1).add(gamingpc).add(workinpc).add(gamingandworkin)
+
+
+replies = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(nb4).add(nb3)
+
+gckb= ReplyKeyboardMarkup(resize_keyboard=True).insert(geoloc).insert(contact).add(contact2)
+
+
+# принятие кнопок
+@dp.callback_query_handler(lambda c: c.data == 'pc', state ='*')
+async def process_callback_button(callback_query: types.CallbackQuery, state: FSMContext):
+    await bot.answer_callback_query(callback_query.id, text='pc')
+    await state.set_state('choicepc')
+    # await message.answer(text='Что выбираешь и для чего?', reply_markup=kb)
+@dp.callback_query_handler(lambda c: c.data == 'mac', state="*")
+async def process_callback_button(callback_query: types.CallbackQuery, state: FSMContext):
+    await bot.answer_callback_query(callback_query.id, text='mac')
+    await state.set_state('mac')
+    # await message.answer(text='Тебе нужен мак!(если деньги есть...)', reply_markup=kb)
+@dp.callback_query_handler(lambda c: c.data == 'cons', state='*')
+async def process_callback_button(callback_query: types.CallbackQuery, state: FSMContext):
+     await bot.answer_callback_query(callback_query.id, text='console')
+     await state.set_state('console')
+     # await message.answer(text='Какой тип консоли?', reply_markup=kb)
+@dp.callback_query_handler(lambda c: c.data == 'vrhelm',state="*")
+async def process_callback_button(callback_query: types.CallbackQuery, state: FSMContext):
+    await bot.answer_callback_query(callback_query.id, text='vr')
+    state.set_state("vr")
+    # await message.answer(text='', reply_markup=kb)
+#основные функции
 async def on_startup():
     print("done!")
 
+# @dp.message_handler(commands=['clear'], state='*')
+# async def clear(message: types.message):
+#     replies = types.ReplyKeyboardRemove()
+#     gckb = types.ReplyKeyboardRemove()
 
 @dp.message_handler(commands=['start'], state="*")
 async def f_c(message: types.Message, state: FSMContext):
@@ -52,63 +128,29 @@ async def f_c(message: types.Message, state: FSMContext):
 
 @dp.message_handler(state='ans')
 async def msg_r(message: types.Message, state: FSMContext):
+    if message.text == 'помощь с выбором':
+        await state.set_state('choice')
+        await message.answer(text='Что выбираешь и для чего?', reply_markup=kb)
+    elif message.text == 'геолокация и контакт':
+        await state.set_state('geo')
+        await message.answer(text='Выбирай', reply_markup=gckb)
 
-    if message.text == 'геолокация и контакт' or message.text == 'помощь с выбором':
-
-        if message.text == 'помощь с выбором':
-            await state.set_state('choice')
-
-        elif message.text == 'геолокация и контакт':
-            await state.set_state('geo')
-    else:
-        None
 
 @dp.message_handler(state='geo')
 async def geo_cont(message: types.Message, state: FSMContext):
-    replies = types.ReplyKeyboardRemove()
-    await message.answer(text='Выбирай',reply_markup=gckb)
     if message.text == 'Вернуться?':
         await state.set_state('ans')
+        await message.answer(text='Что надо', reply_markup=replies)
     else:
         pass
 
 
-@dp.message_handler(state='choice')
-async def choice(message: types.message, state: FSMContext):
-    await message.answer(text='Что выбираешь и для чего?', reply_markup=kb)
+# @dp.message_handler(state='vr')
+# async def choice(message: types.message, state: FSMContext):
+# @dp.callback_query_handler(lambda c: c.data == 'back', state='*')
+# async def process_callback_button(callback_query: types.CallbackQuery):
+#     await bot.answer_callback_query(callback_query.id, text='vr')
 
-
-
-
-@dp.callback_query_handler(lambda callback: callback.data == 'b2',state="*")
-async def process_callback_button(callback_query: types.CallbackQuery, state: FSMContext):
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id
-
-
-@dp.callback_query_handler(lambda c: c.data == 'b1',state='*')
-async def process_callback_button(callback_query: types.CallbackQuery,state: FSMContext):
-    await bot.send_message(callback_query.from_user.id)
-    await bot.answer_callback_query(callback_query.id)
-    await state.set_state('')
-
-@dp.callback_query_handler(lambda callback: callback.data == 'b4', state="*")
-async def process_callback_button(callback_query: types.CallbackQuery, state: FSMContext):
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id
-
-
-@ dp.callback_query_handler(lambda c: c.data == 'b3', state='*')
-def process_callback_button(callback_query: types.CallbackQuery, state: FSMContext):
-
-     await bot.send_message(callback_query.from_user.id)
-     await bot.answer_callback_query(callback_query.id)
-     await state.set_state('mac')
-
-    @dp.callback_query_handler(lambda c: c.data == 'back', state='*')
-    async def process_callback_button(callback_query: types.CallbackQuery):
-        await bot.answer_callback_query(callback_query.id)
-        await bot.send_message(callback_query.from_user.id)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True, on_startup=())
